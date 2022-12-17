1751e0e0bcd0e72b9df7baeafdb3510942fdec85
Pauline Muigai <wanjirumuigai@gmail.com>
2022-12-17 11:17:40 +0300

    completed assignment with errors

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+function submitData(name,email) {
+    fetch('http://localhost:3000/users',{
+    method: 'POST',
+    headers: {
+        
+        "Content-Type": "application/json",
+        //Accept: "application/json",
+    },
+    body: JSON.stringify(
+        {
+            'name': name,
+            'email': email
+        }
+    )
+    })
+    .then(res => res.json()) 
+    .then(data => {
+        console.log(data)
+        let par = document.createAttribute('p').textContent = `${data.id}`
+                      
+        document.querySelector("body").insertAdjacentHTML('beforeend',par)
+       
+    })
+    .catch((err) => {
+        //alert('rejected',err);
+                             
+        document.querySelector("body").insertAdjacentHTML('beforeend',err)
+    });
+}
+submitData("Steve",'steve@steve.com')
